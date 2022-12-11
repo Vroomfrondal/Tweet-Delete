f213174d7e12c5440cda7769cdd1b46b6cb0bdff
Vroomfrondal <lionscantcode@gmail.com>
2022-12-10 23:22:21 -0600

    feat: getUserTweets) pagination
    - getUserTweets() now paginates pages of tweets and returns all of them

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Fresh-Start-Twitter
 
-Do cool things with the twitter API!
+This tool can be used to rapidly delete old tweets for a given timeframe.
 
 ### Dev Details
 
+- [Twitter V2 API](https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api)
 - Utilizes Twitter's `OAuth 1.0a User Context` system to authenticate users based on app keys.
 - More documentation coming soon...
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
 import rwClient from './twitterConfig'
 
-const getUserDetails = async (username: string) => {
-  // must be by specific name. For example. If you're "@JohnSmith", use "JohnSmith"
-  try {
-    const request = await rwClient.v2.userByUsername(username)
-
-    const userData = {
-      ID: request.data.id,
-      UserName: request.data.username,
-      DisplayName: request.data.name,
-    }
-
-    console.log(userData)
-    return userData
-  } catch (e) {
-    console.error(e)
-  }
-}
-
 const newTweet = async (message: string) => {
   try {
     await rwClient.v2.tweet(message)
@@ -38,7 +20,58 @@ const deleteTweet = async (tweetID: string) => {
   return
 }
 
-getUserDetails('chrisdeleon64')
+const getUserDetails = async (username: string) => {
+  // must be by specific name. For example. If you're "@JohnSmith", use "JohnSmith"
+  try {
+    const request = await rwClient.v2.userByUsername(username)
 
+    const userData = {
+      ID: request.data.id,
+      UserName: request.data.username,
+      DisplayName: request.data.name,
+    }
+
+    return userData
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const getUserTweets = async (username: string) => {
+  // Get user details
+  const userTweets: any = []
+  const user = await getUserDetails(username)
+  if (!user) return 'No user for this username found.'
+
+  console.log('Looking for tweets...')
+  const firstRequest = await rwClient.v2.userTimeline(user!.ID)
+  let hasNextPage = true
+  let nextToken = firstRequest.meta.next_token
+
+  while (hasNextPage) {
+    const secondRequest = await rwClient.v2.userTimeline(user!.ID, {
+      pagination_token: nextToken,
+      start_time: undefined,
+      end_time: undefined,
+    })
+    const currToken = secondRequest.meta.next_token
+
+    if (currToken) {
+      const tweets = secondRequest['_realData'].data
+      if (tweets) {
+        userTweets.push(tweets.map((tweet) => tweet.id))
+      }
+
+      nextToken = currToken
+    } else hasNextPage = false
+  }
+
+  if (!userTweets.length) return ['No available tweets for this user.']
+  console.log(userTweets.flat())
+  return userTweets.flat()
+}
+
+getUserTweets('katekuenstller')
+// getUserDetails('chrisdeleon64')
 // newTweet('Test')
 // deleteTweet('')
